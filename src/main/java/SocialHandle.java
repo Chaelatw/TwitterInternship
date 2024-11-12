df9786f009ecf003c482257cdc32f67fbb57ca44
Chaela <chaelawills55@gmail.com>
2024-11-12 09:42:51 -0500

    homework

---
+++ src/main/java/SocialHandle.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+
+public class SocialHandle  {
+
+    HashSet<String> listOfHandles = new HashSet<>();
+
+
+    public String createHandle(String yourSocialHandle) {
+            if(yourSocialHandle.length() > 9){
+            }
+        yourSocialHandle = "@" + yourSocialHandle.toLowerCase();
+
+        return yourSocialHandle;
+    }
+
+
+    public void addHandle(String yourSocialHandle){
+        listOfHandles.add(yourSocialHandle);
+
+    }
+    public int getSizeOfHandles(){
+        return listOfHandles.size();
+    }
+
+
+    public void removeHandle(String yourSocialHandle){
+        listOfHandles.remove(yourSocialHandle);
+    }
+
+
+
+    public HashSet<String> getHandles() {
+        return listOfHandles;
+    }
+
+}
---
+++ src/test/java/SocialHandleTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SocialHandleTest {
+
+    SocialHandle socialHandle;
+
+    @BeforeEach
+    public void setUp(){
+        socialHandle = new SocialHandle();
+    }
+
+
+    @Test
+    void createHandleTest(){
+        String expected = "@coolvideos";
+        String actual = socialHandle.createHandle("coolvideos2024");
+
+    }
+
+    @Test
+    void addHandleTest(){
+        int expected = 1;
+
+        socialHandle.addHandle("@gamerboy25");
+
+        int actual = socialHandle.getSizeOfHandles();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void checkHandleLessThan9CharactersTest(){
+
+
+
+    }
+
+    @Test
+    void removeHandleTest(){
+        int expected = 0;
+         socialHandle.removeHandle("@gamerboy25");
+
+         int actual = socialHandle.getSizeOfHandles();
+
+         assertEquals(expected, actual);
+    }
+}
